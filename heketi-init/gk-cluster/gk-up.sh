aeb849aa5688e9724a1bb19c50a94a6b7926c5e5
Jon Cope <jcope@redhat.com>
2017-08-21 23:09:17 -0500

    Add gfs block create and attach

--- heketi-init/gk-cluster/gk-up.sh
+++ heketi-init/gk-cluster/gk-up.sh
@@ -8,33 +8,7 @@ set -u
 
 source ./gk-common.sh
 
-
-# Dependencies
-DOCKER_DEP="docker-1.12.6"
-FUSE_DEP="glusterfs-fuse"
-UNZIP_DEP="unzip"
-KUBECTL_DEP="https://storage.googleapis.com/kubernetes-release/release/v.1.7.4/bin/linux/amd64/kubectl"
-KUBEADM_REPO="
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF"
-KUBELET_DEP="kubelet"
-KUBEADM_DEP="kubeadm"
-HEKETI_DEP="https://github.com/heketi/heketi/releases/download/v4.0.0/heketi-client-v4.0.0.linux.amd64.tar.gz"
-GK_S3_DEP="https://github.com/jarrpa/gluster-kubernetes/archive/block-and-s3.tar.gz"
-S3_CURL_DEP="http://s3.amazonaws.com/doc/s3-example-code/s3-curl.zip"
-KUBEADM_CONFIG_FLANNELD="--pod-network-cidr=10.244.0.0/16"
-KUBE_FLANNEL="https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml"
-KBUE_FLANNEL_RBAC="https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel-rbac.yml"
-
-############ main  ###############
-echo "-- Gluster-Kubernetes Create --"
+echo "-- Gluster-Kubernetes --"
 echo \
 "This script will deploy a 4 node kubernetes cluster with dependencies for
 testing glsuter-kubernetes and object storage."
@@ -42,26 +16,40 @@ testing glsuter-kubernetes and object storage."
 # Start Up Script
 STARTUP_SCRIPT="$(dirname '${BASH_SOURCE}')/../startup-basic.sh"
 # Create Instance Template
-GCP_TEMPLATE_NAME="${GCP_USER}-gluster-kubernetes"
-${GCLOUD} compute instance-templates create "$GCP_TEMPLATE_NAME" \
+GK_TEMPLATE="${GCP_USER}-gluster-kubernetes"
+${GCLOUD} compute instance-templates create "$GK_TEMPLATE" \
 	--image=$CLUSTER_OS_IMAGE --image-project=$CLUSTER_OS_IMAGE_PROJECT	\
 	--machine-type=$MACHINE_TYPE --network=$GCP_NETWORK \
 	--subnet=$GCP_NETWORK --region=$GCP_REGION  \
 	--boot-disk-auto-delete --boot-disk-size=$NODE_BOOT_DISK_SIZE \
 	--boot-disk-type=$NODE_BOOT_DISK_TYPE --metadata-from-file="startup-script"=$STARTUP_SCRIPT
 # Create node Instance Group
-${GCLOUD} compute instance-groups managed create "$GCP_USER-gk-node"  \
-	--template=$GCP_TEMPLATE_NAME --size=$GK_NUM_NODES --zone=$GCP_ZONE
+GK_GROUP="$GCP_USER-gk-node" 
+${GCLOUD} compute instance-groups managed create $GK_GROUP \
+	--template=$GK_TEMPLATE --size=$GK_NUM_NODES --zone=$GCP_ZONE
+GK_NODE_ARR=($(gcloud compute instance-groups managed list-instances $GK_GROUP | awk 'NR>1{print $1}')) 
+
+#Create GFS Block Devices
+BLK_PREFIX="$GCP_USER-gfs-block"
+for (( i=0; i < ${#GK_NODE_ARR[@]}; i++ )); do
+	GFS_BLK_ARR[$i]="$BLK_PREFIX-$i"
+done
+gcloud compute disks create "${GFS_BLK_ARR[@]}" --size=$GLUSTER_DISK_SIZE --zone=$GCP_ZONE
+
+# Attach GFS Block Devices
+for (( i=0; i < ${#GFS_BLK_ARR[@]}; i++ )); do
+	gcloud compute instances attach-disk ${GK_NODE_ARR[$i]} --disk=${GFS_BLK_ARR[$i]} --zone=$GCP_ZONE
+done
+
 # Create master Instance
-${GCLOUD} compute instances create "$GCP_USER-gk-master" --boot-disk-auto-delete \
+GK_MASTER="$GCP_USER-gk-master"
+${GCLOUD} compute instances create $GK_MASTER --boot-disk-auto-delete \
 	--boot-disk-size=$NODE_BOOT_DISK_SIZE --boot-disk-type=$NODE_BOOT_DISK_TYPE \
 	--image-project=$CLUSTER_OS_IMAGE_PROJECT --machine-type=$MASTER_MACHINE_TYPE \
 	--network=$GCP_NETWORK --zone=$GCP_ZONE --image=$CLUSTER_OS_IMAGE \
 	--metadata-from-file="startup-script"=$STARTUP_SCRIPT
 
-#TODO create GFS block devices
-#TODO attach GFS block devices to nodes
-
+#TODO add cleanup on fail for better idempotency
 #TODO Poll VMs for status RUNNING
 #TODO Poll via SSH kubelet for Ready
 #TODO Poll master for kube node READY
